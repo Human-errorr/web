4c8236221c1a06c0c0367b78006c5903dbf4182e
Fajr <103434966+Human-errorr@users.noreply.github.com>
2022-11-11 11:45:17 +0700

    Delete Website directory

--- Website/Footer.php
+++
@@ -1,6 +0,0 @@
-<footer>
-  <p> &copy; UAS - PEMOGRAMAN WEB I </p>
-  <p> Zikri Hendriyatno (1802096) - Kasih Adinda.S (21112001) - Eka Melisa Pratiwi (21112009) - Fajri Arvandi (21112062) </p>
-</footer>
-
-</html>
